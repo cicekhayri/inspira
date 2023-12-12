e66ebe5b5c81951726d370ba6d3210de5c99a588
Hayri Cicek <hayri.cicek@hiq.se>
2023-12-12 10:04:34 +0100

    Adds middleware assert

---
+++ tests/test_middleware.py
@@ -0,0 +1,28 @@
+from http import HTTPStatus
+
+import pytest
+
+from pyblaze.decorators.http_methods import get
+from pyblaze.enums import HttpMethod
+from pyblaze.responses import JsonResponse
+
+
+@pytest.mark.asyncio
+async def test_middleware(app):
+    async def sample_middleware(request):
+        request.extra_data = "Modified by middleware"
+        return request
+
+    # Use the middleware
+    app.add_middleware(sample_middleware)
+
+    @get("/test")
+    async def test_route(request):
+        return JsonResponse({"message": request.extra_data})
+
+    app.add_route("/test", HttpMethod.GET, test_route)
+
+    response = await app.test_session(app, "GET", "/test")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json()["message"] == "Modified by middleware"
