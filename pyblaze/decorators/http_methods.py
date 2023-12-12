1ba1de6a8b79b902d07b2bcfa5c24b07d2dea2c9
Hayri Cicek <hayri.cicek@hiq.se>
2023-12-12 10:02:10 +0100

    Implements http method decorators

---
+++ pyblaze/decorators/http_methods.py
@@ -0,0 +1,37 @@
+from pyblaze.enums import HttpMethod
+
+
+def get(path: str):
+    def decorator(handler):
+        handler.__method__ = HttpMethod.GET
+        handler.__path__ = path
+        return handler
+
+    return decorator
+
+
+def post(path: str):
+    def decorator(handler):
+        handler.__method__ = HttpMethod.POST
+        handler.__path__ = path
+        return handler
+
+    return decorator
+
+
+def put(path: str):
+    def decorator(handler):
+        handler.__method__ = HttpMethod.PUT
+        handler.__path__ = path
+        return handler
+
+    return decorator
+
+
+def delete(path: str):
+    def decorator(handler):
+        handler.__method__ = HttpMethod.DELETE
+        handler.__path__ = path
+        return handler
+
+    return decorator
