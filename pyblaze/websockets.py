a84448df3eddc760f3e46d1e9f052b50f82d09d4
Hayri Cicek <hayri.cicek@hiq.se>
2023-12-16 19:19:26 +0100

    refactor: Move function to the bottom after classes

--- pyblaze/websockets.py
+++ pyblaze/websockets.py
@@ -1,32 +1,3 @@
-async def handle_websocket(scope, receive, send):
-    path = scope["path"]
-    controller_cls = WebSocketControllerRegistry.get_controller(path)
-
-    if not controller_cls:
-        print(f"No WebSocket controller registered for path: {path}")
-        return
-
-    websocket_cls = WebSocket(scope, receive, send)
-    controller_instance = controller_cls()
-
-    try:
-        await controller_instance.on_open(websocket_cls)
-
-        while True:
-            message = await websocket_cls.receive()
-
-            if message["type"] == "websocket.disconnect":
-                break
-
-            if message["type"] == "websocket.receive":
-                await controller_instance.on_message(websocket_cls, message)
-
-    except Exception as e:
-        print(f"WebSocket connection error: {e}")
-    finally:
-        await controller_instance.on_close(websocket_cls)
-
-
 class WebSocket:
     def __init__(self, scope, receive, send):
         assert scope["type"] == "websocket"
@@ -65,3 +36,32 @@ def websocket(path):
         return cls
 
     return decorator
+
+
+async def handle_websocket(scope, receive, send):
+    path = scope["path"]
+    controller_cls = WebSocketControllerRegistry.get_controller(path)
+
+    if not controller_cls:
+        print(f"No WebSocket controller registered for path: {path}")
+        return
+
+    websocket_cls = WebSocket(scope, receive, send)
+    controller_instance = controller_cls()
+
+    try:
+        await controller_instance.on_open(websocket_cls)
+
+        while True:
+            message = await websocket_cls.receive()
+
+            if message["type"] == "websocket.disconnect":
+                break
+
+            if message["type"] == "websocket.receive":
+                await controller_instance.on_message(websocket_cls, message)
+
+    except Exception as e:
+        print(f"WebSocket connection error: {e}")
+    finally:
+        await controller_instance.on_close(websocket_cls)
