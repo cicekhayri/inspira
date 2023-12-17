b14e67bf7f8df588f16cccbbee082a763a344de1
Hayri Cicek <hayri.cicek@hiq.se>
2023-12-17 09:02:26 +0100

    Added tests for automatic dependency resolution and single dependency resolution functions

---
+++ tests/test_dependency_resolver.py
@@ -0,0 +1,32 @@
+from pyblaze.utils import resolve_dependencies_automatic, resolve_dependency
+
+
+def test_resolve_dependencies_automatic_with_dependencies():
+    class TestClass:
+        def __init__(self, dep1, dep2):
+            pass
+
+    dependencies = resolve_dependencies_automatic(TestClass)
+    assert len(dependencies) == 2
+
+
+def test_resolve_dependencies_automatic_without_dependencies():
+    class TestClass:
+        def __init__(self):
+            pass
+
+    dependencies = resolve_dependencies_automatic(TestClass)
+    assert dependencies is None
+
+
+def test_resolve_dependencies():
+    class TestClass:
+        pass
+
+    dependency_instance = resolve_dependency(TestClass)
+    assert isinstance(dependency_instance, TestClass)
+
+
+def test_resolve_dependency_with_invalid_dependency():
+    invalid_dependency_instance = resolve_dependency(None)
+    assert invalid_dependency_instance is None
