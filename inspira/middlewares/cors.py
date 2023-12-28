2149183c2744eab8c9d46c05c7f3c85e053388e6
Hayri Cicek <hayri.cicek@hiq.se>
2023-12-28 17:32:23 +0100

    Apply automatic code formatting

--- inspira/middlewares/cors.py
+++ inspira/middlewares/cors.py
@@ -19,14 +19,22 @@ class CORSMiddleware:
     async def __call__(self, request: Request):
         origin = request.get_headers().get("origin")
 
-        if origin is not None and (origin not in self.allow_origins and "*" not in self.allow_origins):
+        if origin is not None and (
+            origin not in self.allow_origins and "*" not in self.allow_origins
+        ):
             request.set_forbidden()
             return request
 
         if origin is not None:
             request.set_header("Access-Control-Allow-Origin", origin)
-            request.set_header("Access-Control-Allow-Credentials", str(self.allow_credentials).lower())
-            request.set_header("Access-Control-Allow-Methods", ",".join(self.allow_methods))
-            request.set_header("Access-Control-Allow-Headers", ",".join(self.allow_headers))
+            request.set_header(
+                "Access-Control-Allow-Credentials", str(self.allow_credentials).lower()
+            )
+            request.set_header(
+                "Access-Control-Allow-Methods", ",".join(self.allow_methods)
+            )
+            request.set_header(
+                "Access-Control-Allow-Headers", ",".join(self.allow_headers)
+            )
 
         return request
