451753ddf69e98eee148804f144dc3bdd74bc2ab
Hayri Cicek <hayri.cicek@hiq.se>
2024-01-01 11:12:00 +0100

    Adds default max age function

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,6 @@
+from inspira.config import default_max_age
+
+
 def test_get_item(sample_config):
     assert sample_config["SESSION_COOKIE_NAME"] == "session"
     assert sample_config["INVALID_KEY"] is None
@@ -12,7 +15,7 @@ def test_set_item(sample_config):
 
 
 def test_default_values(sample_config):
-    assert sample_config["SESSION_MAX_AGE"] == 3600
+    assert sample_config["SESSION_MAX_AGE"] == 2678400
     assert sample_config["SESSION_COOKIE_DOMAIN"] is None
     assert sample_config["SESSION_COOKIE_PATH"] is None
     assert sample_config["SESSION_COOKIE_HTTPONLY"] is True
@@ -22,3 +25,9 @@ def test_default_values(sample_config):
 
 def test_unknown_key_returns_none(sample_config):
     assert sample_config["UNKNOWN_KEY"] is None
+
+
+def test_default_max_age():
+    expected_result = 24 * 60 * 60 * 31
+    print(expected_result)
+    assert default_max_age() == expected_result
