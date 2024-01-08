93e1c951d5e963bd5cb71ea0e757dfde0f000bc6
Hayri Cicek <hayri.cicek@hiq.se>
2024-01-08 11:21:23 +0100

    Apply automatic code formatting

--- inspira/utils/inflection.py
+++ inspira/utils/inflection.py
@@ -4,11 +4,12 @@ import inflect
 
 p = inflect.engine()
 
+
 def singularize(word: str) -> str | Literal[False]:
     return p.singular_noun(word) or word
 
 
 def pluralize_word(word: str) -> str:
-    if word.endswith('s'):
+    if word.endswith("s"):
         return word
     return p.plural(word) or word
