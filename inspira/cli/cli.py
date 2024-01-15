ee92e6611512fff57d0f5d2d2bd551dfa7ae0e8f
Hayri Cicek <hayri.cicek@hiq.se>
2024-01-15 11:45:58 +0100

    Refactor: Move function to Utils class for better organization and reusability.

--- inspira/cli/cli.py
+++ inspira/cli/cli.py
@@ -1,13 +1,10 @@
 import click
 
 from inspira.cli.create_app import generate_project
-from inspira.cli.create_controller import create_controller_file, create_src_directory
 from inspira.cli.generate_database_file import create_database_file
-from inspira.cli.generate_model_file import database_file_exists, generate_model_file
-from inspira.cli.generate_repository_file import generate_repository_file
-from inspira.cli.generate_service_file import generate_service_file
-from inspira.migrations.migrations import create_migrations, run_migrations
-from inspira.migrations.utils import get_all_module_names
+from inspira.cli.generate_model_file import database_file_exists
+from inspira.cli.utils import handle_creations, handle_migrations, create_module_files
+
 
 DATABASE_TYPES = ["postgres", "mysql", "sqlite", "mssql"]
 
@@ -63,14 +60,7 @@ def module(name, only_controller, is_websocket):
         click.echo(f"Module '{name}' already exists.")
 
 
-def create_module_files(name, only_controller, is_websocket):
-    create_src_directory()
-    create_controller_file(name, is_websocket)
 
-    if not only_controller:
-        generate_model_file(name)
-        generate_service_file(name)
-        generate_repository_file(name)
 
 
 @new.command()
@@ -113,38 +103,6 @@ def createmigrations(module_name, empty):
     except click.UsageError as e:
         click.echo(f"Error: {e}")
         click.echo("Use 'createmigrations --help' for usage information.")
-
-
-def handle_creations(module_name, empty):
-    """
-    Handle migration creations based on the provided arguments.
-
-    :param module_name: Name of the module for which migrations should be created.
-    :param empty: If provided, generate an empty migration file with the specified name.
-    """
-
-    if empty:
-        module_name = empty[0]
-        migration_name = empty[1]
-    else:
-        migration_name = None
-
-    module_names = [module_name] if module_name else get_all_module_names()
-
-    for module_name in module_names:
-        create_migrations(module_name, migration_name)
-
-
-def handle_migrations(module_name):
-    """
-    Handle migration process based on the provided arguments.
-
-    :param module_name: Name of the module for which migrations should be run.
-    """
-    module_names = [module_name] if module_name else get_all_module_names()
-
-    for current_module_name in module_names:
-        run_migrations(current_module_name)
 
 
 @cli.command()
