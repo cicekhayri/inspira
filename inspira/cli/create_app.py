3d749d8fbee43b31f2135901b6f75ab64e8554e6
Hayri Cicek <hayri.cicek@hiq.se>
2024-01-18 16:34:19 +0100

    Adds function to generate project structure on init

--- inspira/cli/create_app.py
+++ inspira/cli/create_app.py
@@ -2,12 +2,45 @@ import os
 
 import click
 
+from inspira.constants import SRC_DIRECTORY
 from inspira.utils import get_random_secret_key
 
 
 def generate_project():
     create_app_file()
+    create_directory_structure()
+
     click.echo("Project created successfully")
+
+
+def create_directory_structure():
+    base_dir = SRC_DIRECTORY
+    dirs = [
+        base_dir,
+        os.path.join(base_dir, "__init__.py"),
+        os.path.join(base_dir, "controller"),
+        os.path.join(base_dir, "controller", "__init__.py"),
+        os.path.join(base_dir, "model"),
+        os.path.join(base_dir, "model", "__init__.py"),
+        os.path.join(base_dir, "repository"),
+        os.path.join(base_dir, "repository", "__init__.py"),
+        os.path.join(base_dir, "service"),
+        os.path.join(base_dir, "service", "__init__.py"),
+    ]
+
+    for dir_path in dirs:
+        if not os.path.exists(dir_path):
+            if "." in dir_path:
+                module_name = os.path.basename(os.path.dirname(dir_path))
+                import_statement = ""
+
+                if module_name != base_dir:
+                    import_statement = f"from src import {module_name}\n"
+
+                with open(dir_path, 'a') as init_file:
+                    init_file.write(import_statement)
+            else:
+                os.makedirs(dir_path)
 
 
 def create_app_file():
