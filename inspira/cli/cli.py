e1852e1479068aa707a059d7c77ee6cbd0a67d6b
Hayri Cicek <hayri.cicek@hiq.se>
2024-01-18 16:33:29 +0100

    Adds new command for generating controller

--- inspira/cli/cli.py
+++ inspira/cli/cli.py
@@ -1,9 +1,10 @@
 import click
 
 from inspira.cli.create_app import generate_project
+from inspira.cli.create_controller import create_controller_file
 from inspira.cli.generate_database_file import create_database_file
-from inspira.cli.generate_model_file import database_file_exists
-from inspira.cli.utils import create_module_files, handle_creations, handle_migrations
+
+from inspira.migrations.migrations import run_migrations, create_migrations
 
 DATABASE_TYPES = ["postgres", "mysql", "sqlite", "mssql"]
 
@@ -17,43 +18,16 @@ def cli():
 def new():
     pass
 
-
 @new.command()
 @click.argument("name")
-@click.option("--only-controller", "only_controller", is_flag=True, required=False)
 @click.option("--websocket", "is_websocket", is_flag=True, required=False)
-def module(name, only_controller, is_websocket):
-    """
-    Generate files for a new module.
-
-    This command takes a required argument 'name' for the module name, and two optional flags:
-
-    :param str name: Name of the module.
-
-    Optional Flags:
-    :param bool only_controller: If provided, generates only the controller file.
-
-    :param bool is_websocket: If provided, includes WebSocket support in the module.
-
-    Example usage:
-
-    ```\n
-    inspira new module orders --only-controller --websocket\n
-    ```
-
-    This command will generate files for a module named 'orders' with the specified options.
-    """
+def controller(name, is_websocket):
     if not name:
         click.echo("Please provide a name for the module")
         return
 
-    if not only_controller and not database_file_exists():
-        click.echo(
-            "Database file doesn't exist. Please generate one before generating modules"
-        )
-        return
     try:
-        create_module_files(name, only_controller, is_websocket)
+        create_controller_file(name, is_websocket)
         return click.echo(f"Module '{name}' created successfully.")
     except FileExistsError:
         click.echo(f"Module '{name}' already exists.")
@@ -83,34 +57,28 @@ def database(name, type):
 
 
 @cli.command()
-@click.argument("module_name", required=False)
-@click.option(
-    "--empty", nargs=2, type=str, required=False, help="Generate empty migration file."
-)
-def createmigrations(module_name, empty):
+@click.argument("migration_name", required=True)
+def createmigrations(migration_name):
     """
-    Create migrations for the specified module(s).
+    Create migrations
 
-    :param module_name: Name of the module for which migrations should be created.\n
-    :param empty: If provided, generate an empty migration file with the specified name.
+    :param migration_name: Name of the migration e.g. add_column_name_to_order.\n
     """
+
     try:
-        handle_creations(module_name, empty)
+        create_migrations(migration_name)
     except click.UsageError as e:
         click.echo(f"Error: {e}")
         click.echo("Use 'createmigrations --help' for usage information.")
 
 
 @cli.command()
-@click.argument("module_name", required=False)
-def migrate(module_name):
+def migrate():
     """
-    Run migrations for the specified module(s).
-
-    :param module_name: Name of the module for which migrations should be run.
+    Run migrations from the migrations folder.
     """
     try:
-        handle_migrations(module_name)
+        run_migrations()
     except Exception as e:
         click.echo(f"Error: {e}")
         click.echo("Migration failed. Check logs for more details.")
